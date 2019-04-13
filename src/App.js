1d91742454d0ee95a7786d738f23add4359c8860
konradgnat <konradmgnat@gmail.com>
2019-04-13 17:22:00 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import Card from './components/Card';
 
-const LEVEL_SIZE = {
-    'EASY': 12,
-    'MEDIUM': 24,
-    'HARD': 36
-};
-
 const EASY = 'EASY';
 const MEDIUM = 'MEDIUM';
 const HARD = 'HARD';
-const QUIT = 'QUIT';
+
+const LEVEL_SIZE = {
+    EASY: 12,
+    MEDIUM: 24,
+    HARD: 36
+};
+
 
 class App extends Component {
     constructor(props) {
@@ -18,19 +18,28 @@ class App extends Component {
         this.state = {
             game: null,
             selectedCard: null,
+            remaining: null,
             gameCards: [],
             mapIdtoIndex: [],
-            attempts: 0,
+            missed: 0,
             time: 0,
             freeze: false
+        };
+        String.prototype.toMMSS = function () {
+            let sec = parseInt(this, 10);
+            let minutes = Math.floor((sec - (Math.floor(sec / 3600) * 3600)) / 60);
+            let seconds = sec - (Math.floor(sec / 3600) * 3600) - (minutes * 60);
+
+            if (minutes < 10) {minutes = "0"+minutes;}
+            if (seconds < 10) {seconds = "0"+seconds;}
+            return minutes+':'+seconds;
         }
     }
+    tick;
+    gameWonMessage = '';
 
     initGame = (level) => {
-        if (level === QUIT) {
-            this.setState({ game: null, gameCards: [], attempts: 0 });
-            return ;
-        }
+        this.quitGame();
         const cards = [];
         let i = 0;
         const mapIdtoIndex = new Array(LEVEL_SIZE[level]);
@@ -48,8 +57,8 @@ class App extends Component {
             [mapIdtoIndex[i], mapIdtoIndex[j]] = [mapIdtoIndex[j], mapIdtoIndex[i]];
         }
         const flippedMapIdtoIndex = Object.keys(mapIdtoIndex).reduce((o, k) => Object.assign({}, o, { [mapIdtoIndex[k]]: k }), {});
-        this.setState({ game: level, gameCards: cards, mapIdtoIndex: flippedMapIdtoIndex });
-        console.log(mapIdtoIndex, cards);
+        this.setState({ game: level, gameCards: cards, mapIdtoIndex: flippedMapIdtoIndex, remaining: LEVEL_SIZE[level] });
+        this.tick = setInterval(() => this.setState({ time: this.state.time + 1 }), 1000);
     };
 
     renderCards = () => {
@@ -64,7 +73,6 @@ class App extends Component {
         const selected = this.state.selectedCard;
         const cardKey = newCardId % 2 === 1 ? newCardId : newCardId + 1;
         const selectedKey = selected % 2 === 1 ? selected : selected + 1;
-        console.log(newCardId,  selected, cardKey);
         this.showCard(newCardId);
         if (selected !== null) {
             if (selected !== newCardId && selectedKey === cardKey) {
@@ -72,7 +80,7 @@ class App extends Component {
                 this.setState({ freeze: true });
             } else {
                 setTimeout(() => this.hideCards(selected, newCardId), 500);
-                this.setState({ attempts: this.state.attempts + 1, freeze: true });
+                this.setState({ missed: this.state.missed + 1, freeze: true });
             }
             this.setState({ selectedCard: null });
         } else {
@@ -107,8 +115,45 @@ class App extends Component {
         cards[this.state.mapIdtoIndex[cardId]] = (
           <Card key={cardId} cardId={cardId} cardClicked={this.cardClickedHandler} removed={true} flipped={false}/>
         );
-        this.setState({ gameCards: cards, freeze: false, selectedCard: null });
+
+        if (this.state.remaining - 2 <= 0) {
+            this.renderGameWonMessage();
+
+            return;
+        }
+        this.setState({ gameCards: cards, freeze: false, selectedCard: null, remaining: this.state.remaining - 2 });
     };
+
+    renderGameWonMessage() {
+        const finalTime = (this.state.time + '').toMMSS();
+        const finalMissed = this.state.missed;
+        const finalLevel = this.state.game;
+        this.gameWonMessage = (
+          <div className="win_message">
+              <h1>Great job!</h1>
+              <p>Level: {finalLevel}</p>
+              <p>Time: {finalTime}</p>
+              <p>Missed: {finalMissed}</p>
+          </div>
+        );
+        this.setState({ game: null, gameCards: [], missed: 0, remaining: 0, time: 0 });
+        clearInterval(this.tick);
+    }
+
+    renderDashboard() {
+        if (this.state.game === null) {
+            return;
+        }
+        return (
+          <p className="dashboard" >Level: {this.state.game} Time: {(this.state.time +'').toMMSS()} Missed: {this.state.missed}</p>
+        )
+    }
+
+    quitGame() {
+        this.gameWonMessage = '';
+        this.setState({ game: null, gameCards: [], missed: 0, remaining: 0, time: 0 });
+        clearInterval(this.tick);
+    }
 
     render() {
         return (
@@ -118,16 +163,17 @@ class App extends Component {
                         <div className="card blue-grey darken-1">
                             <div className="card-content white-text">
                                 <span className="card-title">Flip-it Memory Card Game</span>
-                                <p>{this.state.game} {this.state.time} {this.state.attempts}</p>
+                                <div className="card-action card__menu">
+                                    <a onClick={() => this.initGame(EASY)} className="waves-effect waves-teal btn-flat">Easy</a>
+                                    <a onClick={() => this.initGame(MEDIUM)} className="waves-effect waves-teal btn-flat">Medium</a>
+                                    <a onClick={() => this.initGame(HARD)} className="waves-effect waves-teal btn-flat">Hard</a>
+                                    <a onClick={() => this.quitGame()} className="waves-effect waves-teal btn-flat">Quit</a>
+                                </div>
+                                {this.renderDashboard()}
                             </div>
                             <div className={`game ${this.state.game}`}>
                                 {this.renderCards()}
-                            </div>
-                            <div className="card-action card__menu">
-                                <a onClick={() => this.initGame(EASY)} className="waves-effect waves-teal btn-flat">Easy</a>
-                                <a onClick={() => this.initGame(MEDIUM)} className="waves-effect waves-teal btn-flat">Medium</a>
-                                <a onClick={() => this.initGame(HARD)} className="waves-effect waves-teal btn-flat">Hard</a>
-                                <a onClick={() => this.initGame(QUIT)} className="waves-effect waves-teal btn-flat">Quit</a>
+                                {this.gameWonMessage}
                             </div>
                         </div>
                     </div>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Easy from './components/Easy';
-import Medium from './components/Medium';
-import Hard from './components/Hard';
 import Header from './components/Header';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
@@ -14,9 +11,6 @@ const routing = (
         <Header />
         <div>
             <Route exact path="/" component={App} />
-            <Route path="/easy" component={Easy} />
-            <Route path="/medium" component={Medium} />
-            <Route path="/hard" component={Hard} />
         </div>
     </BrowserRouter>
 );
